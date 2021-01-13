a837af669d8f154b84defde67404c37f3a6576e1
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2021-01-13 15:47:11 -0800

    final solution of the problem

--- solution.py
+++ solution.py
@@ -5,12 +5,20 @@
 # https://docs.python.org/3/library/re.html
 # https://regexr.com/
 
-regex_integer_in_range = r""
-regex_alternating_repetitive_digit_pair = r""
-
-
 import re
+
+# create regex to validate a 6 digit number from 100000 to 999999
+regex_integer_in_range = r"(^[1-9][0-9]{5}$)"
+
+# create regex to validate alternating repetitive digit
+regex_alternating_repetitive_digit_pair = r"(?=([0-9]).\1)"   
+
+# get the input string
 P = input()
 
+# print true
+# if string match "regex_integer_in_range"
+# if input strig have more than one alternating repetitive digit
+# else false
 print (bool(re.match(regex_integer_in_range, P)) 
 and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
